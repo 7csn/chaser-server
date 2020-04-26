0c57f7eb85171b1b529c5b8f728b8d139a26901b
7csn <1479672753@qq.com>
2020-04-26 13:26:19 +0800

    tcp 协议类 soket 配置补充

--- src/worker/Tcp.php
+++ src/worker/Tcp.php
@@ -25,6 +25,25 @@ class Tcp extends Protocol
         return 'tcp';
     }
 
+    /**
+     * 资源流设置
+     */
+    protected function socketSettings()
+    {
+        if (function_exists('socket_import_stream')) {
+            set_error_handler(function () {
+            });
+            // 转化底层 socket
+            $socket = socket_import_stream($this->socket);
+            // 开启连接状态检测
+            socket_set_option($socket, SOL_SOCKET, SO_KEEPALIVE, 1);
+            // 禁用 tcp 的 Nagle 算法，允许小包数据发送
+            socket_set_option($socket, SOL_TCP, TCP_NODELAY, 1);
+            restore_error_handler();
+        }
+        parent::socketSettings();
+    }
+
     public function reload()
     {
     }
