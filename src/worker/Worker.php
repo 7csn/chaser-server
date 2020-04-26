7666a09c3f02a4f09c440dfd3bdda495ee9f6f26
7csn <1479672753@qq.com>
2020-04-26 22:03:24 +0800

    子进程重装信号处理器

--- src/worker/Worker.php
+++ src/worker/Worker.php
@@ -83,5 +83,42 @@ abstract class Worker implements Countable
     public function run()
     {
         $this->master->setCmdTitle("Chaser[{$this->remit()}]：$this->listening");
+
+        $this->reinstallSignal();
+    }
+
+    /**
+     * 信号处理程序
+     *
+     * @param int $signal
+     */
+    public function signalHandler($signal)
+    {
+        switch ($signal) {
+            case SIGINT:    // 停止
+            case SIGTERM:   // 优雅停止
+            case SIGQUIT:   // 重载
+            case SIGUSR1:   // 优雅重载
+                break;
+            case SIGUSR2:   // 状态
+                break;
+            case SIGIO:     // 连接状态
+                break;
+        }
+    }
+
+    /**
+     * 重装信号处理器
+     */
+    protected function reinstallSignal()
+    {
+        // 终止、优雅终止、重载、优雅重载、状态、连接状态
+        $signals = [SIGINT, SIGTERM, SIGUSR1, SIGQUIT, SIGUSR2, SIGIO];
+        foreach ($signals as $signal) {
+            // 移除旧信号处理
+            pcntl_signal($signal, SIG_IGN, false);
+            // 重装信号处理器
+            $this->reactor->add($signal, Reactor::EV_SIGNAL, [$this, 'signalHandler']);
+        }
     }
 }
